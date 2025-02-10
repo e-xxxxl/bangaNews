6b1113b5fd3a310956577419cefdb27ebe50826c
emmanuel <eajejohnson@gmail.com>
2025-02-10 13:05:12 +0100

    Health section fixed

---
+++ src/Components/LonePages/HealthLonepage/HealthLonepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './HealthLonepage.module.css'; // Import the CSS Module
+import mainImage from '../../../assets/health.jpg'; // Replace with actual image path
+const HealthLonepage = () => {
+  return (
+    <>
+    
+        <div className="container py-5" >
+
+        </div>
+    
+    </>
+  )
+}
+
+export default HealthLonepage
